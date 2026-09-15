6d17bc0e84851a482606943a6906819c788bba7d
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2026-09-15 15:11:17 -0500

    add traitbank docs

---
+++ track.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# tracking ProPollSoil Data Management Plan
+#
+
+latest() {
+ local format=${1:-pdf}
+ preston head\
+ | preston cat\
+ | grep hasVersion\
+ | grep "format=${format}"\
+ | preston cat
+}
+
+preston track \
+ --message "snapshot of TraitBank Data Model Notes" \
+ "https://docs.google.com/document/d/1CFy8WkS3bqVHwjW2bU-rPieMby7JyPV6n3JLCEQwmnM/edit?tab=t.0"
+
+latest pdf \
+ > datamodel.pdf
+
+latest md \
+ > datamodel.md
+
+latest docx \
+ > datamodel.docx
